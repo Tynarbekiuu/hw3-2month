6619b5606094fa00d77e06e0318a81a89aed1ea2
Tynarbekiuu <154976529+Tynarbekiuu@users.noreply.github.com>
2024-01-17 12:16:33 +0300

    Add files via upload
    d8fig7ii

---
+++ homework 3 lesson/script.js
@@ -0,0 +1,250 @@
+const Tyky = {
+    name: 'Tynarbek',
+    age: 17,
+    maritalstatus: 'single',
+    placeofborth: 'osh',
+    school: '40-let',
+    grade: '11th',
+    telephone: 'redmi note9',
+    address: {
+        stereet: 'avenue iuu',
+        apartment: '30',
+        region: 'suvorovskiy',
+        landmark: 'eastern'
+
+    },
+
+    education: [
+        {
+            category: 'masters degree',
+            educationinstitute: 'school',
+            extracurriculumactivities: 'english,progaramming,sport judo,chess,fitness'
+
+        }
+    ],
+    family: [
+        {
+            father: {
+                education: 'masters degree',
+                maritalstatus: 'married',
+                age: '45',
+                hobbies: 'like playing chess',
+                abilities: 'can control the plane',
+                children: '4',
+                phone: 'iphone15promax',
+                number: '1249856309856',
+                placeofborth: 'osh',
+                school: '40-let',
+                grade: 'graduated',
+
+            },
+            mother: {
+                education: 'masters degree',
+                maritalstatus: 'married',
+                age: '42',
+                hobbies: 'like playing chess with my dad',
+                abilities: 'can control the car',
+                children: '4',
+                phone: 'iphone15',
+                number: '1249856342766978',
+                placeofborth: 'osh',
+                school: 'kurmanjan datka',
+                grade: 'graduated',
+
+            },
+            sister1: {
+                education: 'masters degree',
+                maritalstatus: 'married',
+                age: '26',
+                hobbies: 'like doing sport',
+                abilities: 'can control the person',
+                children: '3',
+                phone: 'iphone13',
+                number: '12498563466978',
+                placeofborth: 'osh',
+                school: '40-let',
+                grade: 'graduated',
+
+
+            },
+
+            sister2: {
+                education: 'masters degree',
+                maritalstatus: 'married',
+                age: '24',
+                hobbies: 'like reading the books',
+                abilities: 'can do everything',
+                children: '3',
+                phone: 'iphone13pro',
+                number: '12498563466978',
+                placeofborth: 'osh',
+                school: '40-let',
+                grade: 'graduated',
+
+
+            },
+            brather: {
+                education: 'masters degree',
+                maritalstatus: 'married',
+                age: '22',
+                hobbies: 'like driving cars',
+                abilities: 'can win anyone',
+                children: '2',
+                phone: 'iphone13pro',
+                number: '12498563466978',
+                placeofborth: 'osh',
+                school: 'olimp',
+                grade: 'graduated',
+
+
+            },
+            sisterinlaw: {
+                education: 'masters degree',
+                maritalstatus: 'married',
+                age: '21',
+                hobbies: 'likes cooking',
+                abilities: 'can win with talking',
+                children: '2',
+                phone: 'iphone13pro',
+                number: '12498563466978',
+                placeofborth: 'osh',
+                school: 'huy znaet',
+                grade: 'graduated',
+
+
+            },
+
+
+
+        }
+    ],
+    friends: [
+        {
+
+            information1: {
+                name: 'Baihan',
+                age: 16,
+                maritalstatus: 'single',
+                hobbies: 'likes taking gam',
+                address: {
+                    stereet: 'new jerison',
+                    apartment: '3',
+                    region: 'Nagik',
+                    landmark: 'easternwest'
+                }
+
+            },
+
+            information2: {
+
+                name: 'KYlym',
+                age: 17,
+                maritalstatus: 'single',
+                hobbies: 'he is chert and he also have drivers license',
+                address: {
+                    stereet: 'new orleans',
+                    apartment: '37',
+                    region: 'Nagikko',
+                    landmark: 'zapadnyi'
+
+                }
+
+
+            },
+            information3: {
+                name: 'Isco sisko',
+                age: 17,
+                maritalstatus: 'single',
+                hobbies: 'plays the chess',
+                abilities: 'can intertain the people ',
+                address: {
+                    stereet: '74',
+                    apartment: '17',
+                    region: 'yujnyi',
+                    landmark: 'chynara'
+
+                }
+            },
+            information4: {
+                name: 'Tajik',
+                age: 17,
+                maritalstatus: 'single',
+                hobbies: 'always smokes',
+                abilities: 'can jump from the second floor ',
+                unusualfeatures: 'we call him Tajik because he his eyebrows are attached together',
+                address: {
+                    stereet: '345',
+                    apartment: '14',
+                    region: 'yamaha',
+                    landmark: 'kotex'
+
+                }
+            },
+            information5: {
+                name: 'HOji',
+                age: 17,
+                maritalstatus: 'single',
+                hobbies: 'plays phone games',
+                abilities: 'can drive a car very well ',
+                unusualfeatures: 'he is so hauty',
+                address: {
+                    stereet: 'manhetten',
+                    apartment: 'none',
+                    region: 'osh',
+                    landmark: 'gorodok'
+
+                }
+            },
+            information6: {
+                name: 'Manas',
+                age: 17,
+                maritalstatus: 'married',
+                hobbies: 'likes sleeping',
+                abilities: 'so srtrong ',
+                unusualfeatures: 'has black force',
+                address: {
+                    stereet: 'damadedovo',
+                    apartment: '208347',
+                    region: 'abu dabi',
+                    landmark: 'united arab emirates'
+
+                }
+            },
+            information7: {
+                name: 'kasym joomart',
+                age: 17,
+                maritalstatus: 'married and single we do not know',
+                hobbies: 'likes sleeping',
+                abilities: 'has a very good notion ',
+                unusualfeatures: ' he is crazy',
+                address: {
+                    stereet: 'eco',
+                    apartment: 'iuu',
+                    region: 'abu ali',
+                    landmark: 'united states'
+
+                }
+            },
+            information8: {
+                name: 'Abdunur',
+                age: 17,
+                maritalstatus: 'married ',
+                hobbies: 'likes going to school',
+                abilities: 'no notion at all',
+                unusualfeatures: ' he is young',
+                address: {
+                    stereet: 'toloykon',
+                    apartment: 'auf',
+                    region: 'abu iuu',
+                    landmark: 'united kindom'
+
+                }
+            },
+
+
+        }
+
+    ]
+}
+
+console.log(Tyky.friends[0])
